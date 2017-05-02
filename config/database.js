30caefc0309542406e012a371b13a8b444a9ef02
Rehan <haloworld227@gmail.com>
2017-05-02 07:38:51 +0500

    Beta update 7

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ var Sequelize = require('sequelize');
 var config    = require('./config').development;
 
 var sequelize = new Sequelize(config.database, config.username, config.password, config)
-sequelize.sync({ force: false })
-.then(function() {
-
-})
-.catch(function(err) {
-	console.log(err);
-})
+// sequelize.sync({ force: true })
+// .then(function() {
+//
+// })
+// .catch(function(err) {
+// 	console.log(err);
+// })
 
 module.exports = sequelize;
